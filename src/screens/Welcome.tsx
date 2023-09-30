c95f0ecd79ba19a18ad370edc91d7040bf656be8
Laurens Kropf <laurens.kropf@kropf-it.at>
2023-09-30 22:44:19 +0200

    welcome screen

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import Animated, {FadeInDown} from 'react-native-reanimated';
+import TypeWriter from 'react-native-typewriter';
 import LogoGradient from '../components/LogoGradient';
 
 const Welcome = () => {
@@ -13,9 +15,14 @@ const Welcome = () => {
           left: 0,
           right: 0,
           alignItems: 'center',
-          marginTop: 120,
+          marginTop: 150,
         }}>
-        <Image source={require('./../assets/Text_White.png')} resizeMode="contain" style={{width: 300, height: 150}} />
+        <Animated.Image
+          entering={FadeInDown.duration(1000)}
+          source={require('./../assets/Text_White.png')}
+          resizeMode="contain"
+          style={{width: 300, height: 150}}
+        />
       </View>
 
       <SafeAreaView
@@ -24,20 +31,41 @@ const Welcome = () => {
           left: 0,
           bottom: 0,
           right: 0,
-          alignItems: 'center',
           marginBottom: 60,
+          paddingHorizontal: 30,
         }}>
-        <TouchableOpacity
-          style={{
-            height: 55,
-            width: '90%',
-            backgroundColor: 'white',
-            borderRadius: 10,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text style={{fontWeight: 'bold', fontSize: 17, color: '#883D8C'}}>Los geht's</Text>
-        </TouchableOpacity>
+        <Animated.View entering={FadeInDown.duration(1000).delay(100)}>
+          <TypeWriter typing={1} fixed style={{color: 'white', fontWeight: '600', fontSize: 16}}>
+            Die App für mehr Sicherheit{'\n'}und Privatsphäre im Internet.
+          </TypeWriter>
+          <Animated.View entering={FadeInDown.duration(1000).delay(5000)}>
+            <Text style={{color: 'white', marginTop: 20}}>
+              Mit der Safe im Netz-App kannst du ganz einfach und spielerisch deine Cyber-Sicherheit verbessern.{'\n\n'}
+              Wähle dazu im nächsten Schritt alle Dienste aus, die du verwendest. Die App erstellt dir dann eine
+              Checkliste an Aufgaben, auch Tasks genannt, welche du erledigen (aber auch ignorieren) kannst, um besser
+              gegen die Gefahren im Internet gewappnet zu sein.
+            </Text>
+          </Animated.View>
+        </Animated.View>
+        <Animated.View entering={FadeInDown.duration(1000).delay(6500)}>
+          <TouchableOpacity
+            style={{
+              height: 55,
+              width: '100%',
+              backgroundColor: 'white',
+              borderRadius: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 80,
+            }}>
+            <Text style={{fontWeight: 'bold', fontSize: 17, color: '#883D8C'}}>Los geht's</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{color: 'white', fontSize: 12, textAlign: 'center', marginTop: 10}}>
+              Damit akzeptierst du die <Text style={{textDecorationLine: 'underline'}}>Nutzungsbestimmungen</Text>
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
       </SafeAreaView>
     </View>
   );
