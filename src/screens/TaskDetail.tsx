70e52d5fa3b13139390d77336c2dfdc9b396c8ff
Laurens Kropf <laurens.kropf@kropf-it.at>
2023-10-07 14:45:26 +0200

    finished task detail

--- src/screens/TaskDetail.tsx
+++ src/screens/TaskDetail.tsx
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {ActivityIndicator, Alert, Text, TouchableOpacity, View, useWindowDimensions} from 'react-native';
+import ConfettiCannon from 'react-native-confetti-cannon';
 import RenderHtml from 'react-native-render-html';
 import Button from '../components/Button';
 import CategoryTag from '../components/CategoryTag';
@@ -26,6 +27,8 @@ const TaskDetail = ({route}: {route: any}) => {
   const nav = useNavigation();
 
   const {width} = useWindowDimensions();
+
+  const confettiRef = useRef<any | null>();
 
   useEffect(() => {
     if (route.params?.taskId != null) {
@@ -69,49 +72,58 @@ const TaskDetail = ({route}: {route: any}) => {
   };
 
   const toggleReadStatus = async () => {
+    if (!isDone) {
+      confettiRef.current?.start();
+    }
+
     await _taskService.toggleReadTask(task?.id!);
   };
 
   return (
-    <ScrollViewBackSwipe
-      style={{flex: 1, backgroundColor: Colors.scrollViewBackground}}
-      contentContainerStyle={{paddingTop: 10}}>
-      {task != null && infoContent != null && tutorialContent != null && (
-        <ListView style={{marginVertical: 10, paddingBottom: 40}}>
-          <View
-            style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
-              paddingHorizontal: 20,
-              paddingVertical: 20,
-            }}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>{task?.title}</Text>
-            <CategoryTag text={content?.categories.find(c => c.id === task?.categoryId)!.title} />
+    <View style={{flex: 1}}>
+      <ScrollViewBackSwipe
+        style={{flex: 1, backgroundColor: Colors.scrollViewBackground}}
+        contentContainerStyle={{paddingTop: 10}}>
+        {task != null && infoContent != null && tutorialContent != null && (
+          <ListView style={{marginVertical: 10, paddingBottom: 40}}>
+            <View
+              style={{
+                flexDirection: 'column',
+                justifyContent: 'center',
+                paddingHorizontal: 20,
+                paddingVertical: 20,
+              }}>
+              <Text style={{fontSize: 18, fontWeight: '600'}}>{task?.title}</Text>
+              <CategoryTag text={content?.categories.find(c => c.id === task?.categoryId)!.title} />
+            </View>
+            <View style={{paddingHorizontal: 20, paddingTop: 10, paddingBottom: 20}}>
+              <Text style={{fontSize: 16, fontWeight: '600', marginBottom: 10}}>Information</Text>
+              <RenderHtml contentWidth={width} source={{html: infoContent}} />
+            </View>
+            <View style={{paddingHorizontal: 20, paddingTop: 10, paddingBottom: 20}}>
+              <Text style={{fontSize: 16, fontWeight: '600', marginBottom: 10}}>Tutorial </Text>
+              <RenderHtml contentWidth={width} source={{html: tutorialContent}} />
+            </View>
+            <View style={{paddingHorizontal: 20, paddingVertical: 20}}>
+              <Button
+                title={isDone ? 'Als ungelesen markieren' : 'Als gelesen markieren'}
+                theme="primary"
+                onPress={() => toggleReadStatus()}
+                style={{height: 50}}
+              />
+            </View>
+          </ListView>
+        )}
+        {(task == null || infoContent == null || tutorialContent == null) && (
+          <View style={{alignItems: 'center', paddingTop: 50}}>
+            <ActivityIndicator color={Colors.primary} />
           </View>
-          <View style={{paddingHorizontal: 20, paddingTop: 10, paddingBottom: 20}}>
-            <Text style={{fontSize: 16, fontWeight: '600', marginBottom: 10}}>Information</Text>
-            <RenderHtml contentWidth={width} source={{html: infoContent}} />
-          </View>
-          <View style={{paddingHorizontal: 20, paddingTop: 10, paddingBottom: 20}}>
-            <Text style={{fontSize: 16, fontWeight: '600', marginBottom: 10}}>Tutorial </Text>
-            <RenderHtml contentWidth={width} source={{html: tutorialContent}} />
-          </View>
-          <View style={{paddingHorizontal: 20, paddingVertical: 20}}>
-            <Button
-              title={isDone ? 'Als ungelesen markieren' : 'Als gelesen markieren'}
-              theme="primary"
-              onPress={() => toggleReadStatus()}
-              style={{height: 50}}
-            />
-          </View>
-        </ListView>
-      )}
-      {(task == null || infoContent == null || tutorialContent == null) && (
-        <View style={{alignItems: 'center', paddingTop: 50}}>
-          <ActivityIndicator color={Colors.primary} />
-        </View>
-      )}
-    </ScrollViewBackSwipe>
+        )}
+      </ScrollViewBackSwipe>
+      <View pointerEvents="none" style={{position: 'absolute', top: 0, right: 0, left: 0, bottom: 0}}>
+        <ConfettiCannon ref={confettiRef} count={200} origin={{x: -20, y: 0}} autoStart={false} fadeOut />
+      </View>
+    </View>
   );
 };
 
