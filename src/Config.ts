43a4b681142f5adef7f076767b6e3130f6a64c46
Laurens Kropf <laurens.kropf@kropf-it.at>
2023-10-09 19:09:01 +0200

    updated tos url

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 export const Config = {
   baseUrl: 'https://data.safeimnetz.at',
   websiteUrl: 'https://safeimnetz.at',
-  tosUrl: 'https://safeimnetz.at/nutzungsbedingungen.html',
+  tosUrl: 'https://safeimnetz.at/terms',
   gitHubLink: 'https://github.com/safeimnetz',
   htlWebsiteUrl: 'https://htl-kaindorf.at',
   kropfItWebsiteUrl: 'https://kropf-it.at',
